71ee9c50fa1b677c7b92ba85a35980783b40869b
muness <muness@fe6a39be-99a9-dc11-9a70-001c23e17821>
2008-01-10 05:17:16 +0000

    update version number
    git-svn-id: http://opensource.thinkrelevance.com/svn/spec_converter/trunk@33 fe6a39be-99a9-dc11-9a70-001c23e17821

--- lib/spec_converter.rb
+++ lib/spec_converter.rb
@@ -1,7 +1,7 @@
 # Simple converter to go to test/spec style
 # This will change all files in place, so make sure you are properly backed up and/or committed to SVN!
 class SpecConverter
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
   
   def self.start
     spec_converter = SpecConverter.new
